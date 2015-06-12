150fdfeb3faff798b1a791ce084483ed204e5c81
h3x89 <robert.h3x@gmail.com>
2015-06-12 19:29:32 +0200

    remove modify sidebar

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ class App(pyglet.window.Window):
         super(App, self).__init__(800, 600, "Graph Editor", resizable=True)
         self.set_minimum_size(640, 480)
 
-        self.g = nx.Graph()
+        # self.g = nx.Graph()
+        self.g = nx.DiGraph()
 
         self.mode = "node"
         self.selected = None
@@ -52,7 +53,7 @@ class App(pyglet.window.Window):
             self.help_label = pyglet.text.Label(help_file.read(), multiline=True, x=50, y=self.height - 50,
                     width=self.width-100, height=self.height-100, anchor_y="top", font_name="monospace", font_size=12)
 
-        # load images with anchor center of image
+        # load images with anchor center of image (24x24)
         node_img = pyglet.resource.image("node.png")
         node_img.anchor_x = 12
         node_img.anchor_y = 12
@@ -210,72 +211,73 @@ class App(pyglet.window.Window):
 
             # if mode is modify, then show sidebar
             if self.mode == "modify":
-                if self.selected != None:
-                    attributes = self.g.node[self.selected]
+                None
+                # if self.selected != None:
+                #     attributes = self.g.node[self.selected]
 
                     # variables used not to repeat 100 times the same thing
-                    sidebar_border = 10
-                    sidebar_padding = 20
-                    cell_height = 28
-                    cell_padding = 6
+                    # sidebar_border = 10
+                    # sidebar_padding = 20
+                    # cell_height = 28
+                    # cell_padding = 6
 
                     # precompute some stuff
-                    sidebar_left = self.width - self.sidebar_width - sidebar_border
-                    sidebar_top = self.height - sidebar_border
-                    sidebar_height = sidebar_padding * 2 + len(attributes) * cell_height
-                    sidebar_content_left = sidebar_left + sidebar_padding
-                    sidebar_content_top = sidebar_top - sidebar_padding
-                    cell_width = self.sidebar_width / 2 - sidebar_padding
-                    sidebar_middle = sidebar_content_left + cell_width
-                    sidebar_content_right = sidebar_content_left + cell_width * 2
+                    # sidebar_left = self.width - self.sidebar_width - sidebar_border
+                    # sidebar_top = self.height - sidebar_border
+                    # sidebar_height = sidebar_padding * 2 + len(attributes) * cell_height
+                    # sidebar_content_left = sidebar_left + sidebar_padding
+                    # sidebar_content_top = sidebar_top - sidebar_padding
+                    # cell_width = self.sidebar_width / 2 - sidebar_padding
+                    # sidebar_middle = sidebar_content_left + cell_width
+                    # sidebar_content_right = sidebar_content_left + cell_width * 2
 
                     # draw box
-                    pyglet.graphics.draw(4, pyglet.gl.GL_QUADS,
-                        ('v2f', (sidebar_left, sidebar_top,
-                            sidebar_left + self.sidebar_width, sidebar_top,
-                            sidebar_left + self.sidebar_width, sidebar_top - sidebar_height,
-                            sidebar_left, sidebar_top - sidebar_height)),
-                        ('c3B', (50, 50, 50) * 4)
-                    )
+                    # pyglet.graphics.draw(4, pyglet.gl.GL_QUADS,
+                    #     ('v2f', (sidebar_left, sidebar_top,
+                    #         sidebar_left + self.sidebar_width, sidebar_top,
+                    #         sidebar_left + self.sidebar_width, sidebar_top - sidebar_height,
+                    #         sidebar_left, sidebar_top - sidebar_height)),
+                    #     ('c3B', (50, 50, 50) * 4)
+                    # )
 
                     # draw grid
                     # compute the number of its vertices
-                    vertex_num = 8 + len(attributes) * 2
+                    # vertex_num = 8 + len(attributes) * 2
 
                     # create vertices
-                    grid_vertices = []
+                    # grid_vertices = []
                     # horizontal lines
-                    for n in range(len(attributes) + 1):
-                        grid_vertices.extend((
-                            sidebar_content_left, sidebar_content_top - n * cell_height,
-                            sidebar_content_right, sidebar_content_top - n * cell_height
-                        ))
+                    # for n in range(len(attributes) + 1):
+                    #     grid_vertices.extend((
+                    #         sidebar_content_left, sidebar_content_top - n * cell_height,
+                    #         sidebar_content_right, sidebar_content_top - n * cell_height
+                    #     ))
                     # vertical lines
-                    grid_vertices.extend((
-                        sidebar_content_left, sidebar_content_top,
-                        sidebar_content_left, sidebar_content_top - len(attributes) * cell_height,
-                        sidebar_middle, sidebar_content_top,
-                        sidebar_middle, sidebar_content_top - len(attributes) * cell_height,
-                        sidebar_content_right, sidebar_content_top,
-                        sidebar_content_right, sidebar_content_top - len(attributes) * cell_height,
-                    ))
+                    # grid_vertices.extend((
+                    #     sidebar_content_left, sidebar_content_top,
+                    #     sidebar_content_left, sidebar_content_top - len(attributes) * cell_height,
+                    #     sidebar_middle, sidebar_content_top,
+                    #     sidebar_middle, sidebar_content_top - len(attributes) * cell_height,
+                    #     sidebar_content_right, sidebar_content_top,
+                    #     sidebar_content_right, sidebar_content_top - len(attributes) * cell_height,
+                    # ))
 
                     # actually draw grid
-                    pyglet.graphics.draw(vertex_num, pyglet.gl.GL_LINES,
-                        ('v2f', grid_vertices),
-                        ('c3B', (100, 100, 100) * vertex_num)
-                    )
+                    # pyglet.graphics.draw(vertex_num, pyglet.gl.GL_LINES,
+                    #     ('v2f', grid_vertices),
+                    #     ('c3B', (100, 100, 100) * vertex_num)
+                    # )
 
-                    for n, (key, value) in enumerate(attributes.iteritems()):
-                        ly = sidebar_content_top - n * cell_height - cell_padding
-
-                        key_label = pyglet.text.Label(str(key), font_name='Sans', font_size=12,
-                            x=sidebar_content_left + cell_padding, y=ly, anchor_y="top")
-                        value_label = pyglet.text.Label(str(value), font_name='Sans', font_size=12,
-                            x=sidebar_middle + cell_padding, y=ly, anchor_y="top")
-
-                        key_label.draw()
-                        value_label.draw()
+                    # for n, (key, value) in enumerate(attributes.iteritems()):
+                    #     ly = sidebar_content_top - n * cell_height - cell_padding
+                    #
+                    #     key_label = pyglet.text.Label(str(key), font_name='Sans', font_size=12,
+                    #         x=sidebar_content_left + cell_padding, y=ly, anchor_y="top")
+                    #     value_label = pyglet.text.Label(str(value), font_name='Sans', font_size=12,
+                    #         x=sidebar_middle + cell_padding, y=ly, anchor_y="top")
+                    #
+                    #     key_label.draw()
+                    #     value_label.draw()
 
     def on_mouse_press(self, x, y, buttons, modifiers):
         node = self.check_node(x, y)
@@ -287,22 +289,24 @@ class App(pyglet.window.Window):
             self.selected = None
 
     def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
-        if buttons & mouse.RIGHT:
-            None
-            # self.offset[0] += dx
-            # self.offset[1] += dy
-        elif buttons & mouse.LEFT and self.mode == "modify":
+        # if buttons & mouse.RIGHT:
+        #     None
+        #     # self.offset[0] += dx
+        #     # self.offset[1] += dy
+        # elif buttons & mouse.LEFT and self.mode == "modify":
+        if buttons & mouse.LEFT and self.mode == "modify":
             if self.selected != None:
                 node = self.g.node[self.selected]
 
-                if not self.drag:
-                    True == True
-                    # add to history
-                    # self.history_index += 1
-                    # del self.history[self.history_index:len(self.history)]
-                    # self.history.append(["move", self.selected, copy.copy(node), self.g[self.selected]])
+                # if not self.drag:
+                #     True == True
+                #     # add to history
+                #     # self.history_index += 1
+                #     # del self.history[self.history_index:len(self.history)]
+                #     # self.history.append(["move", self.selected, copy.copy(node), self.g[self.selected]])
+                #     self.drag = True
 
-                    self.drag = True
+                self.drag = True
 
                 node["x"] += dx / self.scale
                 node["y"] += dy / self.scale
